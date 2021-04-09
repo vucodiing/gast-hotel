f2d0b10700e66d02da95a38d3dd70407285adc5b
VuCodiing <vucodiing@gmail.com>
2021-04-09 09:36:22 +0700

    update homepage

--- js/slide.js
+++ js/slide.js
@@ -18,3 +18,35 @@ $(".guest-slide").slick({
   dots: false,
   autoplaySpeed: 2000,
 });
+
+$(".articles-slide").slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 577,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
