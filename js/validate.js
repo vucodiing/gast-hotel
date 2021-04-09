36c5dc262c2a62dfe3882a8f5b7ec23389f9136c
VuCodiing <vucodiing@gmail.com>
2021-04-09 10:32:17 +0700

    update validate homepage

---
+++ js/validate.js
@@ -0,0 +1,28 @@
+$("#home-contact").validate({
+  rules: {
+    homeName: "required",
+    homeCompany: "required",
+    homePhone: {
+      required: true,
+      rangelength: [10, 11],
+      number: true,
+    },
+    homeEmail: {
+      required: true,
+      email: true,
+    },
+  },
+  messages: {
+    homeName: "Please enter Your name",
+    homeCompany: "Please enter Company name",
+    homePhone: {
+      required: "Please enter Your phone",
+      rangelength: "Phone has 10 or 11 numbers",
+      number: "Please enter numbers",
+    },
+    homeEmail: {
+      required: "Please enter Email",
+      email: "Email not true",
+    },
+  },
+});
